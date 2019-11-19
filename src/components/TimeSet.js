dd324a7057a0a776292fbe5053d601f2599d83e5
Emmanuell <emmanuellnogmiq@hotmail.com>
2019-11-19 02:36:39 -0600

    clock input done

--- src/components/TimeSet.js
+++ src/components/TimeSet.js
@@ -1,46 +1,40 @@
 import React, {useState, useEffect} from 'react';
-import moment from "moment";
+import moment, {min} from "moment";
+
 
 const TimeSet = ({type, value}) => {
     const [val, setVal] = value;
-    const [minutes, setMinutes] = useState(0);
-    const [hours, setHours] = useState(0);
-    const handleChanges = (e, type) => {
-        e = parseInt(e, 10);
-        if(isNaN(e)){
-            console.log(typeof e)
-            return;
-        }
-        if (type === 1) {
-            setMinutes(e);
-        } else {
-            setHours(e);
-        }
-        console.log(minutes);
-        console.log(hours);
-        if(!isNaN(hours)){
-            setHours(0);
-        }
-        if(!isNaN(minutes)){
-            setHours(0);
-        }
-        let newTime = parseInt(hours * 60 + minutes, 10);
-        console.log("setting timer " + newTime);
-        setVal(newTime);
-    };
+    const [state, setState] = React.useState({
+        minutes: 0,
+        hours: 0
+    });
+
     useEffect(() => {
-        console.log('Do something after counter has changed', minutes , hours, val);
-    }, [minutes, hours, val]);
+        console.log(state);
+        setVal(parseInt(state.minutes) + parseInt(state.hours ) * 60);
+        console.log("value ", val);
+        console.log('render minutes');
+    }, [state.minutes, state.hours]);
+
+    function handleChange(evt) {
+        const value = evt.target.value;
+        setState({
+            ...state,
+            [evt.target.name]: value
+        });
+        console.log('new state');
+        setVal(parseInt(state.minutes)  + (parseInt(state.hours)*60) * 1000);
+    }
 
     return (
         <div className="control">
             <h2 id={`${type.toLowerCase()}-label`}>{type} Length</h2>
             <span>Minutes</span>
-            <input type="text" onChange={e => handleChanges(e.target.value, 1)}/>
-            {/*<span>Hours</span>*/}
-            {/*<input type="text" onChange={(e) => handleChanges(e.target.value, 2)}/>*/}
+            <input type="text" value={state.minutes} name="minutes" onChange={handleChange}/>
+            <span>Hours</span>
+            <input type="text" value={state.hours} name="hours" onChange={handleChange}/>
         </div>
     )
-}
+};
 
 export default TimeSet
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React , {useEffect} from 'react'
 import moment from 'moment'
-import momentoFormat from 'moment-duration-format'
 
 const Timer = ({currentMode, currentTime}) => {
     const [mode] = currentMode;
     const [time] = currentTime;
+
+
+    useEffect(() => {
+        console.log('time has changed', time);
+    }, [time]);
+
     return (
         <>
             <h2 id="timer-label">{mode === 'session' ? 'Session' : 'Break'}</h2>
             <h3 id="time-left">
-                {
-                    moment.duration(time, "minutes").format("hh:mm")
-                }
-            </h3>
+                {moment.duration(time, 'minutes').hours()}
+                :{moment.duration(time, 'minutes').minutes()}
+                :{moment.duration(time, 'minutes').seconds()}</h3>
         </>
     )
 };
